f4700e9d9b77d4d3604d95a2628e25d800c84139
EgorFurman <egordedkov01@gmail.com>
2024-10-17 15:32:51 +0500

    Added utils module with create_some_matches function to initialize some specified number of test tennis matches

---
+++ utils.py
@@ -0,0 +1,69 @@
+import random
+from random import choice
+
+from app.services import DatabaseService
+
+
+SOME_PLAYERS = (
+    "EvilArthas",
+    "ozon671games",
+    "Глеб Биохакер",
+    "Райан Гослинг",
+    "Мориарти",
+    "Нейробиолог Алипов",
+    "IT Ментор",
+    "Раст Коул",
+    "ПроРОК СанБой",
+    "Тайлер Дерден",
+    "влад пиво",
+    "ivanzolo2004",
+    "Дерек Зуландер",
+    "SHAMAN",
+    "Принцесса Мононоке",
+    "Elon Musk",
+    "Кен Канеки",
+    'Патрик Бэйтмен',
+    "СЕРЕГА ПИРАТ",
+    "Слава КПСС",
+    "Мейби Бейби",
+    "INSTASAMKA",
+    "uebermarginal",
+    "Паша Техник",
+    "Тинькофф",
+    "Уве Болл",
+    "Джейсон Стэйтем"
+)
+
+
+def create_some_matches(count: int = 300):
+    for _ in range(count):
+        player1 = choice(SOME_PLAYERS)
+
+        while True:
+            player2 = choice(SOME_PLAYERS)
+            if player1 == player2:
+                continue
+            break
+
+        match = DatabaseService.add_match(
+            player1, player2
+        )
+
+        winner = random.choice((player1, player2))
+
+        score = match.score
+        score[winner] = {'sets': 2, 'games': 0, 'points': 0}
+
+        DatabaseService.update_match_score_by_uuid(
+            uuid=match.uuid,
+            score=score
+        )
+
+        DatabaseService.update_match_winner_by_uuid(
+            uuid=match.uuid,
+            winner_name=winner
+        )
+
+
+
+
